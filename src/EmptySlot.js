bbb02935e1be9f70bcf08baa3c38d4969ac3aa49
Brandon Dega <brandondega@gmail.com>
2022-06-14 21:31:40 -0500

    added imgs

---
+++ src/EmptySlot.js
@@ -0,0 +1,20 @@
+import { useEffect } from 'react'
+import emptySlot from './molehill.png'
+
+const EmptySlot = (props) => {
+    useEffect(() => {
+        let randSeconds = Math.ceil(Math.random() * 5000)
+        let timer = setTimeout(() => {
+            props.setDisplayMole(true)}, randSeconds)
+
+        return () => clearTimeout(timer)
+    })
+
+    return (
+        <>
+            <img src={emptySlot}/>
+        </>
+    )
+}
+
+export default EmptySlot
